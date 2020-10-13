ec8aba34334468b0ab844302b1d2229c01da3098
Nikolay Dimitriev <dimitrievk56@gmail.com>
2020-10-13 23:35:15 +0300

    fix: при клике на кнопку открывается другая страница

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 var lifehackSlider = new Swiper('.lifehack-container', {
-      spaceBetween: 30,
       centeredSlides: true,
       autoplay: {
-        delay: 2500,
+        delay: 3000,
         disableOnInteraction: false,
       },
       pagination: {
