3a5b3fdf9f6d9aaf192c1aa856a78482bec7740c
Nikolay Dimitriev <dimitrievk56@gmail.com>
2020-10-14 12:31:59 +0300

    написал на js табы в блоке hero

--- js/index.js
+++ js/index.js
@@ -12,4 +12,35 @@ var lifehackSlider = new Swiper('.lifehack-container', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
+});
+
+const hero = document.querySelector('.hero'),
+  heroTabs = hero.querySelectorAll('.hero-tabs__item'),
+  heroSlids = hero.querySelectorAll('.hero-slider__item');
+
+hero.addEventListener('click', function(e) {
+  const target = e.target;
+  let count = 0;
+
+  // если кликнули по табу
+  if (target.closest('.hero-tabs__item')) {
+    //удаляю класс active у всех табов
+    heroTabs.forEach(function(item) {
+      item.classList.remove('hero-tabs__item--active')
     });
+    //добавляю класс active тому, на кого кликнули
+    target.closest('.hero-tabs__item').classList.add('hero-tabs__item--active');
+    // узнаю индекс элемента на который кликнули
+    heroTabs.forEach(function(item, index) {
+      if (item.closest('.hero-tabs__item--active')) {
+        count = index;
+      }
+    });
+    // удаляю класс active у слайдера
+    heroSlids.forEach(function(item) {
+      item.classList.remove('hero-slider__item--active')
+    });
+    // делаю другой слайд активным
+    heroSlids[count].classList.add('hero-slider__item--active');
+  }
+});
